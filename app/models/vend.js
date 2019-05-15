487a878b034466024495266238e71676199a011d
Judah <syluxprime40@gmail.com>
2019-05-15 16:25:26 -0600

    qwertyuiop

---
+++ app/models/vend.js
@@ -0,0 +1,13 @@
+
+
+export default class Can {
+  constructor(name, company, size, sugar, caffeine) {
+    this.name = name,
+    this.company = company,
+    this.caffeine = caffeine,
+    this.sugar = sugar, 
+    this. size = size
+
+  }
+
+}
